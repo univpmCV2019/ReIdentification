308c81f4c1284381099e976e5edd65ed9e088bc2
SaraAbb <saraabbonizio@live.it>
2019-06-17 18:53:42 +0200

    Modificato data_manager

--- losses.py
+++ losses.py
@@ -37,9 +37,7 @@ class CrossEntropyLabelSmooth(nn.Module):
 			targets: ground truth labels with shape (num_classes)
 		"""
 		log_probs = self.logsoftmax(inputs)
-		print(log_probs)
-		print(targets)
-		targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1)
+		targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1), 1)
 		if self.use_gpu: targets = targets.cuda()
 		targets = Variable(targets, requires_grad=False)
 		targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
