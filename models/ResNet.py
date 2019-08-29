04dbfe80e79d8ad064a16928e1d90a73c70a6e3e
SaraAbb <saraabbonizio@live.it>
2019-08-29 18:24:03 +0200

    prova depth

--- models/ResNet.py
+++ models/ResNet.py
@@ -36,7 +36,7 @@ class ResNet50TP(nn.Module):
 		x = x.view(b,t,-1)
 		x = x.permute(0,2,1)
 		f = F.avg_pool1d(x,t)
-		f = f.view(b, self.feat_dim/4)
+		f = f.view(b*2, self.feat_dim/2)
 		
 		#Rete Depth 
 		#1x3x4x3x224x224
@@ -46,7 +46,7 @@ class ResNet50TP(nn.Module):
 		z = z.view(bd,td,-1)
 		z = z.permute(0,2,1)
 		fd = F.avg_pool1d(z,td)
-		fd = fd.view(bd, self.feat_dim/4) #12x2048
+		fd = fd.view(bd*2, self.feat_dim/2) #3x2048
 		
 		#Unire 
 		#sembra aver superato la rete depth...
