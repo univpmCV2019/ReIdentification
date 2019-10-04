ce3e339a9c7671a593ec0a12e12d087a6e8cbfd7
SaraAbb <saraabbonizio@live.it>
2019-10-04 14:04:26 +0200

    setting

--- models/ResNet.py
+++ models/ResNet.py
@@ -29,6 +29,7 @@ class ResNet50TP(nn.Module):
 		#Rete Depth 
 		z = z.view(bd*td,z.size(2), z.size(3), z.size(4))
 		z1 = self.base(z)
+		print(z1.size())
 		z2 = F.avg_pool2d(z1, z1.size()[2:]) #avg pool non ha return_indices
 		z3 = z2.view(bd,td,-1)
 		z4 = z3.permute(0,2,1)
@@ -40,6 +41,7 @@ class ResNet50TP(nn.Module):
 		#Rete base RGB 
 		x = x.view(b*t,x.size(2), x.size(3), x.size(4)) 
 		x = self.base(x)
+		print(x.size())
 		x1 = torch.add(x,z1)
 		x2 = F.avg_pool2d(x1, x1.size()[2:]) #avg pool non ha return_indices
 		x2 = torch.add(x2,z2)
